f3c93d2059a80f6eaeb0a09ea289a321c996cafa
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-24 14:18:13 +0530

    added vpc module path

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+        region = "ap-south-1"
+        access_key = "AKIAZOITTNXULYNR5PNZ"
+        secret_key = "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"
+}
 
+module "vpc" {
+  source = "./vpc"
+}
