6a2be858be6b905962bb2ba23eb02f397c615392
Arpita Patnaik <32553590+arpita497@users.noreply.github.com>
2023-03-26 18:00:24 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-        region = "ap-south-1"
+        region = "ap-southeast-2"
         access_key = "AKIAZOITTNXULYNR5PNZ"
         secret_key = "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"
 }
@@ -11,7 +11,7 @@ module "vpc" {
   name = "terraform-module-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1a", "ap-south-1b"]
+  azs             = ["ap-southeast-2a", "ap-southeast-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
